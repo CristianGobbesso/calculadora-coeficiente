7ba795087e2e679a98d3da4cd279c59b7c81c640
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2026-02-18 22:59:51 -0300

    style: mejorar estructura visual del calculator y jerarquía del resultado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Calculator from "./components/Calculator";
-
+import "./App.css"
 
 function App() {
 
 
   return (
+    <main className="app">
+      <section className="container">
     <Calculator/>
+    </section>
+    </main>
   )
 }
 
