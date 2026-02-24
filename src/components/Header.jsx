8fb176a49b72f2cee8a8d6090d223dbe36b8f20d
Cristian Gobbesso <cristiangobbesso.it@gmail.com>
2026-02-23 21:49:18 -0300

    style(header): mejora efecto hover con animación underline y transición suave

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,32 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="header">
       <div className="header__container">
-        <Link to="/" className="logo">
+        <NavLink to="/" className={({ isActive }) =>
+                  isActive ? "nav__link nav__link--active" : "nav__link"
+                }>
           CDE
-        </Link>
+        </NavLink>
 
         <nav className="nav" aria-label="Navegación principal">
           <ul className="nav__list">
             <li className="nav__item">
-              <Link to="/" className="nav__link">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "nav__link nav__link--active" : "nav__link"
+                }>
                 Inicio
-              </Link>
+              </NavLink>
             </li>
             <li className="nav__item">
-              <Link to="/paginas" className="nav__link">
+              <NavLink to="/paginas" className={({ isActive }) =>
+                  isActive ? "nav__link nav__link--active" : "nav__link"
+                }>
                 Páginas de interés
-              </Link>
+              </NavLink>
             </li>
           </ul>
         </nav>
